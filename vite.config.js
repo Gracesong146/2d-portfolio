c1bf11bf6c6f964dc29d9da0ec59967a181b63a3
Graceee <gracesong146@gmail.com>
2026-02-26 18:53:16 +0800

    Rebuild base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/2d-portfolio/",   // <- repo name, with slashes
   plugins: [react()],
 })
-
-
-export default defineConfig({
-  base: "/2d-portfolio/",   // <- repo name, with slashes
-  // ...rest
-});

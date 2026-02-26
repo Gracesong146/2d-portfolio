ce8e9d780d4002a01ffc24d1ef4c9655972fd13e
Graceee <gracesong146@gmail.com>
2026-02-26 16:24:25 +0800

    Add basic components except Projects not working

--- src/initGame.js
+++ src/initGame.js
@@ -1,16 +1,26 @@
 import makeKaplayCtx from "./kaplayCtx";
 import makePlayer from "./entities/Player";
-//import makeSection from "./components/Section";
+import makeSection from "./components/Section";
 import { PALETTE } from "./constants";
-//import makeSocialIcon from "./components/SocialIcon";
-//import makeSkillIcon from "./components/SkillIcon";
-//import { makeAppear } from "./utils";
-//import makeWorkExperienceCard from "./components/WorkExperienceCard";
-//import makeEmailIcon from "./components/EmailIcon";
-//import makeProjectCard from "./components/ProjectCard";
+import makeSocialIcon from "./components/SocialIcon";
+import makeSkillIcon from "./components/SkillIcon";
+import { makeAppear } from "./utils";
+import makeWorkExperienceCard from "./components/WorkExperienceCard";
+import makeEmailIcon from "./components/EmailIcon";
+import makeProjectCard from "./components/ProjectCard";
 import { cameraZoomValueAtom, store } from "./store";
 
 export default async function initGame() {
+    const generalData = await (await fetch("./configs/generalData.json")).json();
+    const skillsData = await (await fetch("./configs/skillsData.json")).json();
+    const socialsData = await (await fetch("./configs/socialsData.json")).json();
+    const experiencesData = await (
+        await fetch("./configs/experiencesData.json")
+    ).json();
+    const projectsData = await (
+        await fetch("./configs/projectsData.json")
+    ).json();
+    
     const k = makeKaplayCtx();
     k.loadSprite("player", "./sprites/player.png", {
         sliceX: 4,
@@ -93,6 +103,127 @@ export default async function initGame() {
         tiledBackground.height = k.height();
         tiledBackground.uniform.u_aspect = k.width() / k.height();
     });
+
+    makeSection(
+    k,
+    k.vec2(k.center().x, k.center().y - 400),
+    generalData.section1Name,
+    (parent) => {
+      const container = parent.add([k.pos(-805, -700), k.opacity(0)]);
+
+      container.add([
+        k.text(generalData.header.title, { font: "ibm-bold", size: 88 }),
+        k.color(k.Color.fromHex(PALETTE.color1)),
+        k.pos(395, 0),
+        k.opacity(0),
+      ]);
+
+      container.add([
+        k.text(generalData.header.subtitle, {
+          font: "ibm-bold",
+          size: 48,
+        }),
+        k.color(k.Color.fromHex(PALETTE.color1)),
+        k.pos(485, 100),
+        k.opacity(0),
+      ]);
+
+      const socialContainer = container.add([k.pos(130, 0), k.opacity(0)]);
+
+      for (const socialData of socialsData) {
+        if (socialData.name === "Email") {
+          makeEmailIcon(
+            k,
+            socialContainer,
+            k.vec2(socialData.pos.x, socialData.pos.y),
+            socialData.logoData,
+            socialData.name,
+            socialData.address
+          );
+          continue;
+        }
+
+        makeSocialIcon(
+          k,
+          socialContainer,
+          k.vec2(socialData.pos.x, socialData.pos.y),
+          socialData.logoData,
+          socialData.name,
+          socialData.link,
+          socialData.description
+        );
+      }
+
+      makeAppear(k, container);
+      makeAppear(k, socialContainer);
+    }
+  );
+  makeSection(
+    k,
+    k.vec2(k.center().x - 400, k.center().y),
+    generalData.section2Name,
+    (parent) => {
+      /* make the container independent of the section
+       so that the skill icons appear on top of every section's children.
+       so that when the skill icons are pushed around by the player
+       they always remain on top */
+      const container = k.add([
+        k.opacity(0),
+        k.pos(parent.pos.x - 300, parent.pos.y),
+      ]);
+
+      for (const skillData of skillsData) {
+        makeSkillIcon(
+          k,
+          container,
+          k.vec2(skillData.pos.x, skillData.pos.y),
+          skillData.logoData,
+          skillData.name
+        );
+      }
+
+      makeAppear(k, container);
+    }
+  );
+  makeSection(
+    k,
+    k.vec2(k.center().x + 400, k.center().y),
+    generalData.section3Name,
+    (parent) => {
+      const container = parent.add([k.opacity(0), k.pos(0)]);
+      for (const experienceData of experiencesData) {
+        makeWorkExperienceCard(
+          k,
+          container,
+          k.vec2(experienceData.pos.x, experienceData.pos.y),
+          experienceData.cardHeight,
+          experienceData.roleData
+        );
+      }
+
+      makeAppear(k, container);
+    }
+  );
+  makeSection(
+    k,
+    k.vec2(k.center().x, k.center().y + 400),
+    generalData.section4Name,
+    (parent) => {
+      const container = parent.add([k.opacity(0), k.pos(0, 0)]);
+
+      for (const project of projectsData) {
+        makeProjectCard(
+          k,
+          container,
+          k.vec2(project.pos.x, project.pos.y),
+          project.data,
+          project.thumbnail
+        );
+      }
+
+      makeAppear(k, container);
+    }
+  );
 
     makePlayer(k, k.vec2(k.center()), 700);
 }
